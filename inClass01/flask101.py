4bc81f4f968f8679847de999f687380301b6a9e6
wade <johnny12150@gmail.com>
2019-03-09 10:07:50 +0800

    Flask template usage

--- inClass01/flask101.py
+++ inClass01/flask101.py
@@ -17,9 +17,22 @@ def browser():
     user_agent = request.headers.get('User-Agent')
     return f'{user_agent}'
 
+# 使用basic的html template
 @app.route('/about')
 def about():
+    # render_template會自動去templates資料夾下取html
     return render_template('about.html')
+
+# 傳參數到html的template
+@app.route('/tmp/<word>')
+def passVar(word):
+    # 可以接收很多種類型的variables,像是dictionary, list等
+    return render_template('tmp.html', word=word)
+
+# 讀取path(/)
+@app.route('/path/<path:somepath>')
+def showPath(somepath):
+    return '{}'.format(somepath)
 
 if __name__ == '__main__':
     app.run(debug=True)
